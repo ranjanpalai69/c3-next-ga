3ca87cf33e4711c6a15d51d7392775f0c87aaf27
RANJAN PALAI <palairanjan2002@gmail.com>
2023-01-09 14:02:25 +0530

    rct201ev03

--- my-folio/components/Cards.jsx
+++ my-folio/components/Cards.jsx
@@ -2,17 +2,18 @@
 import React from 'react'
 import styles from "../styles/projects.module.css" ;
 const Cards = ({items}) => {
+  
   return (
     <div className={styles.pro}>
        {
             items?.map((item)=>{
-                return <div key={item.id} className={styles.box}>
+                return <div key={item.id} className={styles.box} onClick={()=> window.location.href(`${item.html_url}`)}>
                     <h3 >Project Name:-- {item.name}</h3>
                      <p >Project Url:-{item.html_url}</p>
                      <h3>Star count:- {item.stargazers_count}</h3>
                      <p>Starred Url:-{item.stargazers_url}</p>
                      <h3>Forks count:- {item.forks}</h3>
-                    
+                      <h3>Language:- {item.language}</h3>
                     
                 </div>
             })
